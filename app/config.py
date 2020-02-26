ef8e71be8a1a05926c303743491dfe0b7d415832
Jackson <jacksonsr45@gmail.com>
2020-02-26 15:58:28 -0300

    add setup.py

--- app/config.py
+++ app/config.py
@@ -1,5 +1,5 @@
 conn = {
-    'HEADER_LENGTH' = 10,
+    'HEADER_LENGTH': 10,
     'IP': '0.0.0.0',
     'PORT': 8000,
 }
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+
+setup(
+   name='chatbot_python',
+   version='1.0',
+   description='A simple chat bot with python.',
+   license="MIT",
+   author='jacksonsr45@gmail.com',
+   author_email='jacksonsr45@gmail.com',
+   url="http://gnjsistemas.com/",
+   packages=[
+        'app',
+        'teste',
+   ],  #same as name
+   install_requires=[
+        'socket',
+        'select',
+        'errno',], #external packages as dependencies
+   scripts=[
+       
+           ]
+)
