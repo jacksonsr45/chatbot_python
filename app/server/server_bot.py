bca7466242c94b6a7e8fe8ea91a4bf314aaab5b8
Jackson <jacksonsr45@gmail.com>
2020-02-27 10:36:19 -0300

    Update function __run_server in class Server

--- app/server/server_bot.py
+++ app/server/server_bot.py
@@ -6,67 +6,72 @@ from app.config import conn
 
 class Server:
     def __init__(self):
-        HEADER_LENGTH = conn['HEADER_LENGTH']
-        IP = conn['IP']
-        PORT = conn['PORT']
+        self.HEADER_LENGTH = conn['HEADER_LENGTH']
+        self.IP = conn['IP']
+        self.PORT = conn['PORT']
+        self.sockets_list = []
+        self.clients = {}
 
-        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-        server_socket.bind((IP, PORT))
+        self.server_socket.bind((self.IP, self.PORT))
 
-        server_socket.listen()
+        self.server_socket.listen()
 
-        sockets_list = []
-        sockets_list.append(server_socket)
+        self.sockets_list.append(self.server_socket)
 
-        clients = {}
+        self.__run_server()
 
-        def receive_message(client_socket):
-            try:
-                message_header = client_socket.recv(HEADER_LENGTH)
-
-                if not len(message_header):
-                    return False
-
-                message_length = int(message_header.decode("utf-8").strip())
-                return {'header': message_header, 'data': client_socket.recv(message_length)}
-            except:
-                return False
-
+        
+    def __run_server(self):
         while True:
-            read_sockets, _, exception_socket = select.select( sockets_list, [], sockets_list)
+            read_sockets, _, exception_socket = select.select( self.sockets_list, [], self.sockets_list)
 
             for notified_socket in read_sockets:
-                if notified_socket == server_socket:
-                    client_socket, client_address = server_socket.accept()
-                    user = receive_message(client_socket)
+                if notified_socket == self.server_socket:
+                    self.client_socket, self.client_address = self.server_socket.accept()
+                    self.user = self.receive_message(self.client_socket)
 
-                    if user is False:
+                    if self.user is False:
                         continue
 
-                    sockets_list.append(client_socket)
+                    self.sockets_list.append(self.client_socket)
 
-                    clients[client_socket] = user
-                    print(f"Accepde new connection from {client_address[0]}:{client_address[1]} username: {user['data'].decode('utf-8')}")
+                    clients[self.client_socket] = self.user
+                    print(f"Accepde new connection from {self.client_address[0]}:{self.client_address[1]} username: {self.user['data'].decode('utf-8')}")
 
                 else:
-                    message = receive_message(notified_socket)
+                    self.message = self.receive_message(notified_socket)
 
-                    if message is False:
+                    if self.message is False:
                         print(f"Closed connection from {clients[notified_socket]['data'].decode('utf-8')}")
-                        sockets_list.remove(notified_socket)
+                        self.sockets_list.remove(notified_socket)
                         del clients[notified_socket]
                         continue
 
-                    user = clients[notified_socket]
-                    print(f"Received message from {user['data'].decode('utf-8')}:{message['data'].decode('utf-8')}")
+                    self.user = clients[notified_socket]
+                    print(f"Received message from {self.user['data'].decode('utf-8')}:{self.message['data'].decode('utf-8')}")
 
-                    for client_socket in clients:
-                        if client_socket != notified_socket:
-                            client_socket.send(user['header'] + user['data'] + message['header'] + message['data'])
+                    for self.client_socket in clients:
+                        if self.client_socket != notified_socket:
+                            self.client_socket.send(self.user['header'] + self.user['data'] + self.message['header'] + self.message['data'])
                             
             for notified_socket in exception_socket:
-                sockets_list.remove(notified_socket)
-                del client_socket[notified_socket]
+                self.sockets_list.remove(notified_socket)
+                del self.client_socket[notified_socket]
+
+    
+    
+    def receive_message(client_socket):
+        try:
+            message_header = client_socket.recv(self.HEADER_LENGTH)
+
+            if not len(message_header):
+                return False
+
+            message_length = int(message_header.decode("utf-8").strip())
+            return {'header': message_header, 'data': client_socket.recv(message_length)}
+        except:
+            return False
